e3d5a8903c1521ab7438912d54618bb5e516875d
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-29 14:43:43 +0900

    finished draft of fun fact app

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <small>© 2022  Made with ❤️ by Nikki Carlos. All rights reserved.</small>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img className="nav-logo" src="./logo192.png" alt="logo" />
+      <h1>ReactFacts</h1>
+      <h3>Project 1</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
